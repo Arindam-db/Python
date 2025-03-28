6892e135ce052081c16be9027684c62cdda7fe97
Arindam Deb <debarindam225@gmail.com>
2025-03-28 17:46:12 +0530

    second commit

---
+++ Exercise01.py
@@ -0,0 +1,8 @@
+# find the area of a rectangle
+
+length = float(input("enter the length of the rectangle: "))
+breadth = float(input("enter the breadth of the rectangle: "))
+
+area = length * breadth
+
+print(f"the area of the rectangle is {area}")
---
+++ userinput.py
@@ -0,0 +1,8 @@
+# input() is a function that prompts the user for input and returns the data as a string 
+
+name = input("what is your name ? ")
+age = int(input("how old are you ? "))
+age += 1
+
+print(f"Hello {name} " )
+print(f"you are {age} years old")
